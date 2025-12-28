7b4b93c184b6ca629e78b2700699039fde9bb912
Lionel Schiepers <lionel_schiepers@hotmail.com>
2025-12-28 03:10:59 +0100

    api has been moved to dedicated repo

--- api/build.cmd
+++
@@ -1,8 +0,0 @@
-docker build -t lionelschiepers/stockquote-node-api -f .\Dockerfile .
-docker push lionelschiepers/stockquote-node-api:latest
-
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock nickfedor/watchtower --cleanup --run-once
-
-@REM automatically update the docker container using watchtower
-@REM sudo docker run -d --name=watchtower -v /var/run/docker.sock:/var/run/docker.sock --restart=always -e WATCHTOWER_POLL_INTERVAL=3600 nickfedor/watchtower --cleanup
-@REM sudo docker run --rm -v /var/run/docker.sock:/var/run/docker.sock nickfedor/watchtower --cleanup --run-once
