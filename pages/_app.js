f2b416907efb747f3686dfc02cd4f94febf0d5f5
Lionel Schiepers <lionel_schiepers@hotmail.com>
2026-01-02 00:46:30 +0100

    sonarqube fix

--- pages/_app.js
+++ pages/_app.js
@@ -24,12 +24,12 @@ import Head from 'next/head';
 import PropTypes from 'prop-types';
 
 const getInitialTheme = () => {
-  if (typeof window !== 'undefined') {
+  if (typeof globalThis.window !== 'undefined') {
     const savedTheme = localStorage.getItem('theme');
     if (savedTheme) {
       return savedTheme === 'dark';
     }
-    return window.matchMedia('(prefers-color-scheme: dark)').matches;
+    return globalThis.window.matchMedia('(prefers-color-scheme: dark)').matches;
   }
   return false;
 };
